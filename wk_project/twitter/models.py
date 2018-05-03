19dfbe8a5577231986ba37099014e5632812b932
MajBog <maja.boguslawska@gmail.com>
2018-05-03 21:59:07 +0200

    models changed: Likes&Image option

--- wk_project/twitter/models.py
+++ wk_project/twitter/models.py
@@ -7,6 +7,8 @@ from django.db import models
 class Message(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=250)
+    image = models.ImageField(null=True)
+
     date = models.DateField()
 
 class Comment(models.Model):
@@ -18,6 +20,11 @@ class Comment(models.Model):
 class Following(models.Model):
     follower = models.OneToOneField(User, on_delete=models.CASCADE)
     followed = models.ManyToManyField(User, related_name="followed_user")
+
+
+class Like(models.Model):
+    who_likes = models.ForeignKey(User, on_delete=models.CASCADE)
+    what_likes = models.ForeignKey(Message, on_delete=models.CASCADE)
 
 
 
